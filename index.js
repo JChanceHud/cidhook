0c053fd56a41ee091a9d431e8281f57667dca426
Chance Hudson <jchancehud@gmail.com>
2019-01-18 21:53:38 -0600

    Request https by default

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const fetch = require('node-fetch');
 const isIPFS = require('is-ipfs');
 
-function assertArgs(domain, cid) {
+function parseArgs(_domain, cid) {
   return Promise.resolve()
     .then(() => {
-      if (isIPFS.multihash(cid)) return;
-      throw new Error(`Non-multihash cid supplied: ${cid}`);
+      if (!isIPFS.multihash(cid)) {
+        throw new Error(`Non-multihash cid supplied: ${cid}`);
+      }
+      const domain = _domain.indexOf('http') === 0 ? _domain : `https://${_domain}`
+      return { cid, domain };
     });
 }
 
-function pin(domain, cid) {
-  return assertArgs(domain, cid)
-    .then(() => fetch(`${domain}/${cid}`, {
+function pin(_domain, _cid) {
+  return parseArgs(_domain, _cid)
+    .then(({ domain, cid }) => fetch(`${domain}/${cid}`, {
       method: 'POST',
       headers: {
         'Authorization': process.env.CIDHOOK_SECRET
@@ -23,9 +26,9 @@ function pin(domain, cid) {
     });
 }
 
-function unpin(domain, cid) {
-  return assertArgs(domain, cid)
-    .then(() => fetch(`${domain}/${cid}`, {
+function unpin(_domain, _cid) {
+  return parseArgs(_domain, _cid)
+    .then(({ domain, cid}) => fetch(`${domain}/${cid}`, {
       method: 'DELETE',
       headers: {
         'Authorization': process.env.CIDHOOK_SECRET
