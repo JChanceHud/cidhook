bc8cfe7ac834b3669b235bcbe5c583f4ca52bdd9
Chance Hudson <jchancehud@gmail.com>
2019-01-24 03:04:13 -0600

    Switch to yargs for argument parsing

--- cmd.js
+++ cmd.js
@@ -1,31 +1,27 @@
 #!/usr/bin/env node
 
 const cidhook = require('.');
-const [ _, __, domain, cid, method ] = process.argv;
 
-const { CIDHOOK_SECRET } = process.env;
+require('yargs')
+  .command('pin <server> <cid>', 'pin an item', yargs =>
+    yargs
+      .positional('cid', {
+        describe: 'the content id to be pinned'
+      })
+      .positional('server', {
+        describe: 'the cidhookd server url, example: http://localhost:3000'
+      }),
+    argv => cidhook.pin(argv.server, argv.cid))
+  .command(
+    'unpin <server> <cid>', 'unpin an item', yargs =>
+      yargs
+        .positional('cid', {
+          describe: 'the content id to be unpinned'
+        })
+        .positional('server', {
+          describe: 'the cidhookd server url, example: http://localhost:3000'
+        }),
+    argv => cidhook.unpin(argv.server, argv.cid))
+  .argv
 
-if (process.argv.length < 3) {
-  console.log('\nUsage: cidhook <url> <cid> [pin|unpin]\n');
-  process.exit(0);
-}
-
-Promise.resolve()
-  .then(() => {
-    if (!method || method === 'pin') {
-      return cidhook.pin(domain, cid);
-    } else if (method === 'unpin') {
-      return cidhook.unpin(domain, cid);
-    } else {
-      console.log(`Invalid command specified ${method}`);
-      process.exit(1);
-    }
-  })
-  .then(() => {
-    console.log(`cid ${cid} successfully updated: (${method || 'pin'})`);
-    process.exit(0);
-  })
-  .catch(err => {
-    console.log(err);
-    process.exit(1);
-  });
+  // console.log(`cid ${cid} successfully updated: (${method || 'pin'})`);
