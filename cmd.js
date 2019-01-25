d3bc0d18ccb1d9b0f412b218443e8050a059ec82
Chance Hudson <jchancehud@gmail.com>
2019-01-25 11:55:21 -0600

    Print output in commands, use await

--- cmd.js
+++ cmd.js
@@ -9,14 +9,20 @@ yargs
       .positional('cid', {
         describe: 'the content id to be pinned'
       }),
-    argv => cidhook.pin(argv.server, argv.cid))
+    async argv => {
+      await cidhook.pin(argv.server, argv.cid);
+      console.log(`cid ${argv.cid} successfully updated: (pin)`);
+    })
   .command(
     'unpin <cid>', 'unpin an item', _yargs =>
       _yargs
         .positional('cid', {
           describe: 'the content id to be unpinned'
         }),
-    argv => cidhook.unpin(argv.server, argv.cid))
+    async argv => {
+      await cidhook.unpin(argv.server, argv.cid);
+      console.log(`cid ${argv.cid} successfully updated: (unpin)`);
+    })
   .option('server', {
     alias: 's',
     default: 'http://localhost:3000'
